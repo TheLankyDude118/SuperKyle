d32b375f93997ea3e8b4b5a9ec1f36785a354cef
TheLankyDude118 <jaden.lawrence11@gmail.com>
2022-09-03 13:00:39 +0100

    all features of game ready

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -5,9 +5,30 @@ using UnityEngine;
 public class CameraControl : MonoBehaviour
 {
     [SerializeField] private Transform player;
+    private float camX = 0;
+    private float camY = 0;
 
     private void Update()
     {
-        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+        if (player.position.y >= -10.73)
+        {
+            camY = player.position.y;
+        }
+        else
+        {
+            camY = transform.position.y;
+        }
+
+        if (player.position.x >= 0)
+        {
+            camX = player.position.x;
+        }
+        else
+        {
+            camY = transform.position.y;
+        }
+
+        transform.position = new Vector3(camX, camY, transform.position.z);
+
     }
 }
--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
-    [SerializeField] private float speed;
+    [SerializeField] private float speed = 2;
     [SerializeField] private float degrees;
     private void Update()
     {
